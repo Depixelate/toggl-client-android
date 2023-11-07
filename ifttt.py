dfbf7209e95daa7bb8bc26f92fdf6527e1669cd2
Sukessh Velusamy <sukesshvelusamy@gmail.com>
2023-11-07 17:55:44 +0530

    Initial commit

---
+++ ifttt.py
@@ -0,0 +1,33 @@
+"""
+Function(s) to deal with IFTTT Webhooks
+"""
+import logging
+from pprint import pformat
+import time
+import requests
+
+IFTTT_KEY = "ebnnyeYuoXIQPVETP3csWrF1FzIwfZH6nQRtYpnu6UH"
+IFTTT_DELAY = 5
+REQUEST_TIMEOUT = 10
+
+
+def phone_notification():
+    """
+    triggers the phone alarm via IFTTT webhook
+    """
+    while True:
+        try:
+            logging.info("Calling IFTTT Webhook...")
+            data = requests.get(
+                f"https://maker.ifttt.com/trigger/pomo_done/with/key/{IFTTT_KEY}",
+                timeout=REQUEST_TIMEOUT,
+            )
+
+            logging.info("IFTTT Response: %s", pformat(vars(data)))
+            break
+
+        except Exception as _:  # pylint:disable=W0718
+            logging.exception(
+                "Error While Making IFTTT Request! Trying again in 5 seconds..."
+            )
+            time.sleep(IFTTT_DELAY)
